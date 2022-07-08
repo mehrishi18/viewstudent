e65ba50fd9797fe5f5148d2dcabc8a0fd6328bea
mehrishi18 <sahrishikesh8@gmail.com>
2022-07-08 10:15:03 +0530

    message

---
+++ src/app/studentname/studentname.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-studentname',
+  templateUrl: './studentname.component.html',
+  styleUrls: ['./studentname.component.css']
+})
+export class StudentnameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  name="hrishi"
+
+}
