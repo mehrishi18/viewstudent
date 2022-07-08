da7a71987845898da4bd8438a36bc6cb2342dc52
mehrishi18 <sahrishikesh8@gmail.com>
2022-07-08 12:04:09 +0530

    student details

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,7 +13,7 @@ describe('AppComponent', () => {
       ],
     }).compileComponents();
   });
-
+  ("name":"hrishi","age":19)
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
